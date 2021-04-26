2a137f627386fd59b0a8f77595e47e1c017d461b
Facundo Yagodnig <facundoyagodnig@gmail.com>
2021-04-26 13:37:37 -0300

    cambiamos el puerto

--- node_js/express_basic/index.js
+++ node_js/express_basic/index.js
@@ -53,6 +53,8 @@ app.get("/data", (req, res) => {
   res.json(cursos);
 });
 
-app.listen(5000, () => {
-  console.log("Server running in port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`);
 });
